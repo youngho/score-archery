6e533a367e0c8510e66ab572ca53fea8f63ace12
youngho <xfile0304@gmail.com>
2026-02-10 16:16:37 +0900

    타이머 변경

--- Assets/Script/BalloonTimer.cs
+++ Assets/Script/BalloonTimer.cs
@@ -14,14 +14,15 @@ public class BalloonTimer : MonoBehaviour
     [Tooltip("0이 되면 로드할 씬 이름 (상위 메뉴)")]
     public string menuSceneName = "00StartUI";
 
+    [SerializeField] private TextMeshProUGUI _timerText;
+
     private float _remainingSeconds;
-    private TextMeshProUGUI _timerText;
-    private GameObject _timerCanvasRoot;
 
     private void Start()
     {
         _remainingSeconds = startSeconds;
-        CreateTimerUI();
+        if (_timerText == null)
+            _timerText = GameObject.Find("TimerText")?.GetComponent<TextMeshProUGUI>();
         UpdateDisplay();
     }
 
@@ -50,31 +51,6 @@ public class BalloonTimer : MonoBehaviour
         recorder.RecordCurrentStageAndThen(LoadMenuScene);
     }
 
-    private void CreateTimerUI()
-    {
-        _timerCanvasRoot = new GameObject("BalloonTimerCanvas");
-        var canvas = _timerCanvasRoot.AddComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        canvas.sortingOrder = 100;
-
-        _timerCanvasRoot.AddComponent<UnityEngine.UI.CanvasScaler>();
-        _timerCanvasRoot.AddComponent<UnityEngine.UI.GraphicRaycaster>();
-
-        var textGo = new GameObject("TimerText");
-        textGo.transform.SetParent(_timerCanvasRoot.transform, false);
-
-        _timerText = textGo.AddComponent<TextMeshProUGUI>();
-        var textRt = _timerText.rectTransform;
-        textRt.anchorMin = new Vector2(0.5f, 1f);
-        textRt.anchorMax = new Vector2(0.5f, 1f);
-        textRt.pivot = new Vector2(0.5f, 1f);
-        textRt.anchoredPosition = new Vector2(0f, -40f);
-        textRt.sizeDelta = new Vector2(200f, 80f);
-        _timerText.fontSize = 64f;
-        _timerText.alignment = TextAlignmentOptions.Center;
-        _timerText.color = Color.white;
-    }
-
     private void UpdateDisplay()
     {
         if (_timerText != null)
@@ -84,16 +60,6 @@ public class BalloonTimer : MonoBehaviour
     private void LoadMenuScene()
     {
         if (string.IsNullOrEmpty(menuSceneName)) return;
-
-        if (_timerCanvasRoot != null)
-            Destroy(_timerCanvasRoot);
-
         SceneManager.LoadScene(menuSceneName, LoadSceneMode.Single);
-    }
-
-    private void OnDestroy()
-    {
-        if (_timerCanvasRoot != null)
-            Destroy(_timerCanvasRoot);
     }
 }
