dd7cdad4d22c82b975d57529268e186cb1254120
youngho <xfile0304@gmail.com>
2026-03-25 18:52:44 +0900

    Scarecrow 수정

--- Assets/Script/01Scarecrow/ScarecrowBehavior.cs
+++ Assets/Script/01Scarecrow/ScarecrowBehavior.cs
@@ -15,30 +15,6 @@ public class ScarecrowBehavior : MonoBehaviour
 
     private void TryHandleArrowHit(Collider other)
     {
-        // 화살(ArcheryArrow)인지 확인
-        ArcheryArrow arrow = other.GetComponentInParent<ArcheryArrow>();
-        if (arrow == null) return;
-
-        // 화살의 Rigidbody / Collider 가져오기
-        Rigidbody arrowRb = arrow.GetComponent<Rigidbody>();
-        Collider arrowCol = arrow.GetComponent<Collider>();
-
-        // 화살 물리 멈추기
-        if (arrowRb != null)
-        {
-            arrowRb.linearVelocity = Vector3.zero;
-            arrowRb.angularVelocity = Vector3.zero;
-            arrowRb.isKinematic = true;
-        }
-
-        // 더 이상 다른 것과 충돌하지 않도록 콜라이더 비활성화
-        if (arrowCol != null)
-        {
-            arrowCol.enabled = false;
-        }
-
-        // 허수아비에 붙이기
-        arrow.transform.SetParent(transform);
 
         // 점수 추가
         if (ScoreManager.Instance != null)
